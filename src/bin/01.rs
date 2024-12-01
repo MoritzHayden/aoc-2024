9d234a50dd001a2bd083623bdd21ba4d628aad6e
Hayden Moritz <Moritz.Hayden@gmail.com>
2024-12-01 00:26:06 -0600

    feat: start day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,30 @@
+use regex::Regex;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    // 1. Parse the input into two lists using regex capturing groups
+    let mut total_diff: u32 = 0;
+    let mut left_locations: Vec<u32> = Vec::new();
+    let mut right_locations: Vec<u32> = Vec::new();
+    let re = Regex::new(r"(\d+)\s+(\d+)").unwrap();
+    for line in input.lines() {
+        let caps = re.captures(line).unwrap();
+        left_locations.push(caps[1].parse::<u32>().unwrap());
+        right_locations.push(caps[2].parse::<u32>().unwrap());
+    }
+
+    // 2. Sort the lists
+    left_locations.sort();
+    right_locations.sort();
+
+    // 3. Iterate through the lists and calculate the difference between the two corresponding elements
+    for (left, right) in left_locations.iter().zip(right_locations.iter()) {
+        total_diff += left - right;
+    }
+
+    // 4. Sum the differences and return the total difference
+    Some(total_diff)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
