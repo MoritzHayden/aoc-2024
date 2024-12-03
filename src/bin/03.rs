6821280d2ade7764e2d4623a07cd8b9603e6d214
Hayden Moritz <moritz.hayden@gmail.com>
2024-12-03 11:15:31 -0600

    feat: scaffold day 3 part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -2,13 +2,21 @@ use regex::Regex;
 
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u32> {
+enum Part {
+    One,
+    Two,
+}
+
+fn calculate_product(input: &str, part: Part) -> Option<u32> {
     // Define variables
     let mut product: u32;
     let mut result: u32 = 0;
 
     // Extract each mul instruction using multiple regex capturing groups
-    let re = Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\))").unwrap();
+    let re = match part {
+        Part::One => Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\))").unwrap(),
+        Part::Two => Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\))").unwrap(),
+    };
 
     // Calculate the product of each mul instruction
     for cap in re.captures_iter(input) {
@@ -22,8 +30,12 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(result)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u32> {
+    Some(calculate_product(input, Part::One)?)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(calculate_product(input, Part::Two)?)
 }
 
 #[cfg(test)]
