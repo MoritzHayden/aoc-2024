349a58f319f7474fe447ee4dbc299191b343346c
Hayden Moritz <moritz.hayden@gmail.com>
2024-12-05 10:13:30 -0600

    feat: refactor day 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -64,41 +64,91 @@ fn get_middle(updates: &[u32]) -> u32 {
     updates[middle]
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    // Parse input into rules and updates
-    let mut sum_pages: u32 = 0;
-    let rules = parse_rules(input);
-    let updates = parse_updates(input);
-
-    // Loop through the updates from right to left and check the rules
-    for update in &updates {
-        let mut is_valid_update: bool = true;
-        for i in (0..update.len()).rev() {
-            // If the update is not valid, mark it as such and break
-            if rules.contains_key(&update[i]) {
-                for j in (0..i).rev() {
-                    if rules[&update[i]].contains(&update[j]) {
-                        is_valid_update = false;
-                        break;
-                    }
+fn is_valid_update(rules: &HashMap<u32, Vec<u32>>, update: &[u32]) -> bool {
+    // Loop through the update from right to left and check the rules
+    for i in (0..update.len()).rev() {
+        // If the update is not valid, return false
+        if rules.contains_key(&update[i]) {
+            for j in (0..i).rev() {
+                if rules[&update[i]].contains(&update[j]) {
+                    return false;
                 }
             }
-            if !is_valid_update {
-                break;
-            }
-        }
-
-        // If the update is valid, add the middle page to the sum
-        if is_valid_update {
-            sum_pages += get_middle(update);
         }
     }
 
+    // If the update is valid, return true
+    true
+}
+
+fn get_matching_updates(
+    rules: &HashMap<u32, Vec<u32>>,
+    updates: &Vec<Vec<u32>>,
+    valid: bool,
+) -> Vec<Vec<u32>> {
+    let mut matching_updates: Vec<Vec<u32>> = vec![];
+
+    // Loop through each update and check if it is valid
+    for update in updates {
+        // If the update is not valid, return false
+        if (is_valid_update(rules, update) && valid) || (!is_valid_update(rules, update) && !valid)
+        {
+            matching_updates.push(update.to_vec());
+        }
+    }
+
+    // If the update is valid, return true
+    matching_updates
+}
+
+fn correct_update(rules: &HashMap<u32, Vec<u32>>, update: &[u32]) -> Vec<u32> {
+    let mut corrected_update: Vec<u32> = vec![];
+
+    // Construct the corrected update
+    for page in update {
+        // TODO: Replace with corrected page
+        corrected_update.push(*page);
+    }
+
+    // Return the corrected update
+    corrected_update
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    // Parse input into rules and updates
+    let mut sum_pages: u32 = 0;
+    let rules: HashMap<u32, Vec<u32>> = parse_rules(input);
+    let updates: Vec<Vec<u32>> = parse_updates(input);
+
+    // Get the valid updates
+    let valid_updates: Vec<Vec<u32>> = get_matching_updates(&rules, &updates, true);
+
+    // Sum the middle pages of each valid update
+    for valid_update in valid_updates {
+        sum_pages += get_middle(&valid_update);
+    }
+
+    // Return the sum of valid middle pages
     Some(sum_pages)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    // Parse input into rules and updates
+    let mut sum_pages: u32 = 0;
+    let rules: HashMap<u32, Vec<u32>> = parse_rules(input);
+    let updates: Vec<Vec<u32>> = parse_updates(input);
+
+    // Get the invalid updates
+    let invalid_updates: Vec<Vec<u32>> = get_matching_updates(&rules, &updates, false);
+
+    // Correct the ordering of each invalid update and sum the middle pages
+    for invalid_update in invalid_updates {
+        let corrected_update: Vec<u32> = correct_update(&rules, &invalid_update);
+        sum_pages += get_middle(&corrected_update);
+    }
+
+    // Return the sum of corrected middle pages
+    Some(sum_pages)
 }
 
 #[cfg(test)]
@@ -114,6 +164,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
